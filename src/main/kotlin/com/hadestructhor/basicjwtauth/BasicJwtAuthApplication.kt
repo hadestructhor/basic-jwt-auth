3d49f08c2ceba77f232193abd81ff26a2562f32f
hadestructhor <angelo_alyacoub@hotmail.fr>
2021-10-16 14:58:03 +0200

    Initialization

---
+++ src/main/kotlin/com/hadestructhor/basicjwtauth/BasicJwtAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.hadestructhor.basicjwtauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicJwtAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<BasicJwtAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/hadestructhor/basicjwtauth/BasicJwtAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hadestructhor.basicjwtauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicJwtAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
